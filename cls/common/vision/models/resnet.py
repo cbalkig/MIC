cfdefc57ae179c67e44cb76705ddc738d6f65c19
Cavide Balkı Gemirter <cavide.balki@gmail.com>
2025-12-24 15:47:06 +0300

    Init

--- cls/common/vision/models/resnet.py
+++ cls/common/vision/models/resnet.py
@@ -1,7 +1,7 @@
 
 import torch.nn as nn
 from torchvision import models
-from torchvision.models.utils import load_state_dict_from_url
+from torch.hub import load_state_dict_from_url
 #from torch.hub import load_state_dict_from_url
 from torchvision.models.resnet import BasicBlock, Bottleneck, model_urls
 import copy
